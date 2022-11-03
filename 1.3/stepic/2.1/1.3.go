8335bb6f529691c959d345154528df0d9b9dff0f
Aleksei Kolmogorov <sunriseman33@gmail.com>
2022-11-03 13:38:32 +1000

    feet: added log

---
+++ 1.3/stepic/2.1/1.3.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	var (
+		a int
+		b int
+		c int
+		d int
+	)
+	fmt.Scan(&a, &b, &c, &d)
+
+	//fmt.Println(minimumFromFour(a, b, c, d))
+
+	// func vote(x int, y int, z int) int {
+	// fmt.Scan(&x,&y,&z)
+}
